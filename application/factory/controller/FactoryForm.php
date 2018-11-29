7d935d438e54fc5a405de5f12f111facef41ef74
xiaojun91 <778275690@qq.com>
2018-11-29 18:41:57 +0800

    no commit message

---
+++ application/factory/controller/FactoryForm.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\factory\controller;
+use app\common\controller\FormBase;
+
+class FactoryForm extends FormBase
+{
+    function __construct(){
+        parent::__construct();
+    }
+}
