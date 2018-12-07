fb3a34e7d1dbfc678f9c432833f78398bbaeb340
依然→流暢 <liuchang_best@163.com>
2018-12-07 10:59:57 +0800

    no commit message

--- wja_form_field.sql
+++ wja_form_field.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 06/12/2018 20:37:45
+ Date: 07/12/2018 10:59:53
 */
 
 SET NAMES utf8mb4;
@@ -77,7 +77,7 @@ CREATE TABLE `wja_form_model`  (
   `add_time` int(13) UNSIGNED NULL DEFAULT NULL,
   `update_time` int(13) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`model_id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of wja_form_model
@@ -87,6 +87,13 @@ INSERT INTO `wja_form_model` VALUES (2, '厂商管理', 'store_factory', '', 1, 
 INSERT INTO `wja_form_model` VALUES (3, '角色管理', 'user_group', '', 1, 0, NULL, 1544096207, 1544096207);
 INSERT INTO `wja_form_model` VALUES (4, '用户管理', 'user', '', 1, 0, NULL, 1544098575, 1544098575);
 INSERT INTO `wja_form_model` VALUES (5, '商品管理', 'goods', '', 1, 0, NULL, 1544099050, 1544099050);
+INSERT INTO `wja_form_model` VALUES (6, '商品分类', 'goods_cate', '', 1, 0, NULL, 1544146973, 1544146973);
+INSERT INTO `wja_form_model` VALUES (7, '商品规格', 'goods_spec', '', 1, 0, NULL, 1544147492, 1544147492);
+INSERT INTO `wja_form_model` VALUES (8, '渠道管理', 'store_channel', '', 1, 0, NULL, 1544147751, 1544147751);
+INSERT INTO `wja_form_model` VALUES (9, '零售商管理', 'store_dealer', '', 1, 0, NULL, 1544148310, 1544148310);
+INSERT INTO `wja_form_model` VALUES (10, '服务商管理', 'store_servicer', '', 1, 0, NULL, 1544148899, 1544148899);
+INSERT INTO `wja_form_model` VALUES (11, '安装员管理', 'user_installer', '', 1, 0, NULL, 1544149645, 1544149645);
+INSERT INTO `wja_form_model` VALUES (12, '返佣明细表', 'commission', '', 1, 0, NULL, 1544100306, 1544100306);
 
 -- ----------------------------
 -- Table structure for wja_form_table
@@ -109,7 +116,7 @@ CREATE TABLE `wja_form_table`  (
   `add_time` int(13) UNSIGNED NULL DEFAULT NULL,
   `update_time` int(13) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`table_id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 30 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 74 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of wja_form_table
@@ -143,5 +150,49 @@ INSERT INTO `wja_form_table` VALUES (26, '5', 'thumb', '产品图片', '100', 5,
 INSERT INTO `wja_form_table` VALUES (27, '5', 'goods_sn', '产品货号', '100', 1, NULL, '', 70, 1, 0, 0, 0, 1544099311, 1544099311);
 INSERT INTO `wja_form_table` VALUES (28, '5', 'min_price', '产品价格', '100', 1, NULL, '', 80, 1, 0, 0, 0, 1544099348, 1544099348);
 INSERT INTO `wja_form_table` VALUES (29, '5', 'goods_stock', '产品库存', '100', 1, NULL, '', 90, 1, 0, 0, 0, 1544099396, 1544099396);
+INSERT INTO `wja_form_table` VALUES (30, '6', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544147221, 1544147221);
+INSERT INTO `wja_form_table` VALUES (31, '6', 'sname', '厂商名称', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544147262, 1544147262);
+INSERT INTO `wja_form_table` VALUES (32, '6', 'name', '分类名称', '*', 1, NULL, '', 30, 1, 0, 0, 0, 1544147341, 1544147341);
+INSERT INTO `wja_form_table` VALUES (33, '7', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544147514, 1544147514);
+INSERT INTO `wja_form_table` VALUES (34, '7', 'sname', '厂商名称', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544147539, 1544147539);
+INSERT INTO `wja_form_table` VALUES (35, '7', 'name', '规格名称', '100', 1, NULL, '', 30, 1, 0, 0, 0, 1544147583, 1544147660);
+INSERT INTO `wja_form_table` VALUES (36, '7', 'value', '规格属性', '*', 1, NULL, '', 40, 1, 0, 0, 0, 1544147605, 1544147605);
+INSERT INTO `wja_form_table` VALUES (37, '8', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544147775, 1544147775);
+INSERT INTO `wja_form_table` VALUES (38, '8', 'region_name', '负责区域', '*', 1, NULL, '', 50, 1, 0, 0, 0, 1544147856, 1544148085);
+INSERT INTO `wja_form_table` VALUES (39, '8', 'caution_money', '保证金金额', '100', 1, NULL, '', 60, 1, 0, 0, 0, 1544147888, 1544148090);
+INSERT INTO `wja_form_table` VALUES (40, '2', 'domain', '二级域名', '100', 1, NULL, '', 15, 1, 0, 0, 0, 1544147968, 1544147968);
+INSERT INTO `wja_form_table` VALUES (41, '8', 'name', '渠道名称', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544148133, 1544148133);
+INSERT INTO `wja_form_table` VALUES (42, '8', 'user_name', '联系人姓名', '100', 1, NULL, '', 70, 1, 0, 0, 0, 1544148175, 1544148175);
+INSERT INTO `wja_form_table` VALUES (43, '8', 'phone', '联系电话', '150', 1, NULL, '', 80, 1, 0, 0, 0, 1544148200, 1544148269);
+INSERT INTO `wja_form_table` VALUES (44, '8', 'username', '管理员账号', '100', 1, NULL, '', 90, 1, 0, 0, 0, 1544148223, 1544148223);
+INSERT INTO `wja_form_table` VALUES (45, '9', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544148337, 1544148337);
+INSERT INTO `wja_form_table` VALUES (46, '9', 'name', '零售商名称', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544148370, 1544148370);
+INSERT INTO `wja_form_table` VALUES (47, '9', 'cname', '所属渠道', '*', 1, NULL, '', 30, 1, 0, 0, 0, 1544148442, 1544148442);
+INSERT INTO `wja_form_table` VALUES (48, '8', 'sname', '所属厂商', '*', 1, NULL, '', 15, 1, 0, 0, 0, 1544148510, 1544148510);
+INSERT INTO `wja_form_table` VALUES (49, '9', 'sname', '所属厂商', '*', 1, NULL, '', 25, 1, 0, 0, 0, 1544148663, 1544148663);
+INSERT INTO `wja_form_table` VALUES (50, '9', 'user_name', '联系人姓名', '100', 1, NULL, '', 50, 1, 0, 0, 0, 1544148759, 1544148759);
+INSERT INTO `wja_form_table` VALUES (51, '9', 'phone', '联系电话', '150', 1, NULL, '', 60, 1, 0, 0, 0, 1544148785, 1544148785);
+INSERT INTO `wja_form_table` VALUES (52, '9', 'username', '管理员账号', '100', 1, NULL, '', 70, 1, 0, 0, 0, 1544148823, 1544148823);
+INSERT INTO `wja_form_table` VALUES (53, '10', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544148997, 1544148997);
+INSERT INTO `wja_form_table` VALUES (54, '10', 'name', '服务商名称', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544149017, 1544149017);
+INSERT INTO `wja_form_table` VALUES (55, '10', 'sname', '所属厂商', '*', 1, NULL, '', 30, 1, 0, 0, 0, 1544149033, 1544149033);
+INSERT INTO `wja_form_table` VALUES (56, '10', 'user_name', '联系人姓名', '100', 1, NULL, '', 40, 1, 0, 0, 0, 1544149061, 1544149061);
+INSERT INTO `wja_form_table` VALUES (57, '10', 'phone', '联系电话', '150', 1, NULL, '', 50, 1, 0, 0, 0, 1544149082, 1544149082);
+INSERT INTO `wja_form_table` VALUES (58, '10', 'username', '管理员账号', '100', 1, NULL, '', 60, 1, 0, 0, 0, 1544149108, 1544149108);
+INSERT INTO `wja_form_table` VALUES (59, '11', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544149679, 1544149679);
+INSERT INTO `wja_form_table` VALUES (60, '11', 'fname', '所属厂商', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544149723, 1544149723);
+INSERT INTO `wja_form_table` VALUES (61, '11', 'sname', '所属服务商', '*', 1, NULL, '', 30, 1, 0, 0, 0, 1544149748, 1544149748);
+INSERT INTO `wja_form_table` VALUES (62, '11', 'region_name', '服务区域', '*', 1, NULL, '', 40, 1, 0, 0, 0, 1544150166, 1544150166);
+INSERT INTO `wja_form_table` VALUES (63, '11', 'realname', '真实姓名', '100', 1, NULL, '', 50, 1, 0, 0, 0, 1544150207, 1544150241);
+INSERT INTO `wja_form_table` VALUES (64, '11', 'phone', '联系电话', '150', 1, NULL, '', 70, 1, 0, 0, 0, 1544150234, 1544150234);
+INSERT INTO `wja_form_table` VALUES (65, '11', 'udata_id', '绑定小程序', '100', 2, NULL, 'yorn', 80, 1, 0, 0, 0, 1544150592, 1544150600);
+INSERT INTO `wja_form_table` VALUES (66, '12', '', '编号', '60', 3, NULL, '', 10, 1, 0, 0, 0, 1544100500, 1544100500);
+INSERT INTO `wja_form_table` VALUES (67, '12', 'order_sn', '订单编号', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544100538, 1544100538);
+INSERT INTO `wja_form_table` VALUES (68, '12', 'gname', '商品名称', '*', 1, NULL, '', 20, 1, 0, 0, 0, 1544100572, 1544100572);
+INSERT INTO `wja_form_table` VALUES (69, '12', 'sname', '零售商名称', '*', 1, NULL, '', 30, 1, 0, 0, 0, 1544100592, 1544101029);
+INSERT INTO `wja_form_table` VALUES (70, '12', 'order_amount', '订单金额', '80', 1, NULL, '', 40, 1, 0, 0, 0, 1544100610, 1544100610);
+INSERT INTO `wja_form_table` VALUES (71, '12', 'commission_ratio', '佣金百分比', '100', 1, NULL, '', 50, 1, 0, 0, 0, 1544100633, 1544100633);
+INSERT INTO `wja_form_table` VALUES (72, '12', 'income_amount', '佣金金额', '80', 1, NULL, '', 60, 1, 0, 0, 0, 1544100654, 1544100654);
+INSERT INTO `wja_form_table` VALUES (73, '12', 'add_time', '交易时间', '120', 2, NULL, 'date', 70, 1, 0, 0, 0, 1544100679, 1544100679);
 
 SET FOREIGN_KEY_CHECKS = 1;
