ad3d0ab148bb89ff461aeaa91ca6d3b02915438b
jinzhou <694995669@qq.com>
2019-01-07 19:12:33 +0800

    新增系统菜单

--- config/sysmenu.php
+++ config/sysmenu.php
@@ -8,4 +8,5 @@ return [
     ['name' => '公司动态', 'url' => '/CompanyDynamic', 'page_type' => 1,'sort'=>2],//
     ['name' => '渠道入驻', 'url' => '/JoinInvestment/Register', 'page_type' => 1,'sort'=>3],
     ['name' => '零售网点', 'url' => '/RetailOutlets', 'page_type' => 1,'sort'=>4],
+    ['name' => '渠道服务', 'url' => '/ServiceChannels', 'page_type' => 1,'sort'=>3],
 ];
