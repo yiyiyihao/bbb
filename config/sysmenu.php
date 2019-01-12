646e9e4f0d7fac9c83f1a5cc1fabc6bb15ed4bc8
jinzhou <694995669@qq.com>
2019-01-12 10:04:06 +0800

    菜单

--- config/sysmenu.php
+++ config/sysmenu.php
@@ -6,7 +6,7 @@
 return [
     ['name' => '首页', 'url' => '/', 'page_type' => 1,'sort'=>1],
     ['name' => '公司动态', 'url' => '/CompanyDynamic', 'page_type' => 1,'sort'=>2],//
-    //['name' => '渠道入驻', 'url' => '/JoinInvestment/Register', 'page_type' => 1,'sort'=>3],
-    ['name' => '零售网点', 'url' => '/RetailOutlets', 'page_type' => 1,'sort'=>4],
+    ['name' => '渠道入驻', 'url' => '/JoinInvestment/Register', 'page_type' => 1,'sort'=>3],
+    //['name' => '零售网点', 'url' => '/RetailOutlets', 'page_type' => 1,'sort'=>4],
     ['name' => '渠道服务', 'url' => '/ServiceChannels', 'page_type' => 1,'sort'=>3],
 ];
