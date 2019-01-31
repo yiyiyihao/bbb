16babbed848103e9e5316599c80f95691bf9ecd8
Joe3160 <694995669@qq.com>
2019-01-31 12:19:18 +0800

    异步先不分页

--- application/factory/controller/Help.php
+++ application/factory/controller/Help.php
@@ -18,40 +18,11 @@ class Help extends CommonHelp
         //dump($this->adminStore);
     }
 
-    private function query($param=[])
-    {
-        $alias=isset($param['alias'])?$param['alias']:$this->_getAlias();
-        $query=$this->model;
-        if ($alias) {
-            $query->alias($alias);
-        }
-        $field=isset($param['field'])?$param['field']:$this->_getField();
-        if ($field) {
-            $query->field($field);
-        }
-        $join=isset($param['join'])?$param['join']:$this->_getJoin();
-        if ($join) {
-            $query->join($join);
-        }
-        $where=isset($param['where'])?$param['where']:$this->_getWhere();
-        if ($where) {
-            $query->where($where);
-        }
-        $order=isset($param['order'])?$param['order']:$this->_getOrder();
-        if ($order) {
-            $query->order($order);
-        }
-        $data=$query->select();
-        return $data;
-    }
+    
 
 
     public function getList()
     {
-        //$data=$this->query(['field'=>'H.title,H.answer']);
-        //$this->ajaxJsonReturn($data);
-        //return;
-
         $where=$this->_getWhere();
         $join=$this->_getJoin();
         $alias=$this->_getAlias();
