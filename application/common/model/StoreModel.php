0d3b3fb4757174554d1ccbd1d5f9bc3f193f08e3
Joe3160 <1050333+joe3160@users.noreply.gitee.com>
2019-03-11 10:38:01 +0800

    返回键

--- application/common/model/StoreModel.php
+++ application/common/model/StoreModel.php
@@ -18,6 +18,37 @@ class StoreModel extends Model
     protected $name='store';
     protected $createTime = 'add_time';
     protected $updateTime = 'update_time';
-    protected $autoWriteTimestamp = false;
+    protected $autoWriteTimestamp = true;
+
+
+    //public static function init()
+    //{
+    //    self::event('after_write', function ($store) {
+    //        if (!isset($store['store_no']) || empty($store['store_no'])) {
+    //            self::getMchKey($store['store_type']);
+    //            return false;
+    //        }
+    //    });
+    //}
+
+
+    private static function getMchKey($storeType = 1)
+    {
+        //商户编号：商户类型+“年月日”+“4位随机数字”
+        $key = $storeType.date('ymd').get_nonce_str(4, 2);
+        //判断当日新增商户数量是否超过9999个
+        $beginToday = mktime(0,0,0,date('m'),date('d'),date('Y')); //今日开始时间戳
+        $exist = db('store')->where(['store_type' => $storeType, 'add_time' => ['>=', $beginToday]])->count();
+        if ($exist >= 9999) {
+            return FALSE;
+        }
+        //判断商户编号是否存在
+        $info = db('store')->where(['store_no' => $key])->find();
+        if ($info){
+            return self::getMchKey($storeType);
+        }else{
+            return $key;
+        }
+    }
 
 }
