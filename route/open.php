3828687a187897938d0428c90118a8ba286dcd30
Joe3160 <1050333+joe3160@users.noreply.gitee.com>
2019-03-12 17:50:34 +0800

    no commit message

---
+++ route/open.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * User: Administrator
+ * Date: 2019/3/11 0011
+ * Time: 12:14
+ */
+
+Route::group('v10', function () {
+    Route::get('demo', 'demo/index');
+    //工单管理
+    Route::group('work_order', function () {
+        //工单列表
+        Route::any('/index', 'v10.work_order/index');
+        //工单详情
+        Route::any('/detail', 'v10.work_order/detail');
+    });
+});
