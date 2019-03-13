4c2266d50ac7392d644c7577709b293c433619e1
Joe3160 <1050333+joe3160@users.noreply.gitee.com>
2019-03-13 20:43:34 +0800

    no commit message

--- route/open.php
+++ route/open.php
@@ -6,19 +6,23 @@
  * Time: 12:14
  */
 
-Route::group('v10', function () {
-    Route::get('demo', 'demo/index');
-    //工单管理
-    Route::group('work_order', function () {
-        //工单列表
-        Route::any('index', 'v10.work_order/index');
-        //工单详情
-        Route::any('detail', 'v10.work_order/detail');
-        //取消工单
-        Route::any('cancel', 'v10.work_order/cancel');
-        //评价工单
-        Route::any('assess', 'v10.work_order/assess');
-        //评价配置
-        Route::any('assess_config', 'v10.work_order/assessConfig');
+Route::domain('open', function () {
+    Route::group('v10', function () {
+        Route::get('demo', 'open/demo/index');
+        //工单管理
+        Route::group('work_order', function () {
+            //工单列表
+            Route::any('index', 'v10.work_order/index');
+            //工单详情
+            Route::any('detail', 'v10.work_order/detail');
+            //取消工单
+            Route::any('cancel', 'v10.work_order/cancel');
+            //评价工单
+            Route::any('assess', 'v10.work_order/assess');
+            //评价配置
+            Route::any('assess_config', 'v10.work_order/assessConfig');
+            //提交维修工单
+            Route::any('add', 'v10.work_order/add');
+        });
     });
 });
