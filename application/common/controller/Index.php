031ee01c7a798eaf640bda3b7cbad7acdc72549c
xiaojun91 <685549+xiaojun91@users.noreply.gitee.com>
2019-05-05 09:47:45 +0800

    no commit message

--- application/common/controller/Index.php
+++ application/common/controller/Index.php
@@ -16,7 +16,6 @@ class Index extends CommonBase
     public function index($template = '',$url = '')
     {
         $menuList = $this->getMenu();
-        pre($menuList);
         $this->assign('menuList', $menuList);
         config('app_trace',false);
         return $this->fetch($template);
