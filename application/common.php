2c2b6f8f0ae4af260724965a40c5cd8668da2aa1
Joe3160 <1050333+joe3160@users.noreply.gitee.com>
2019-05-08 11:53:56 +0800

    no commit message

--- application/common.php
+++ application/common.php
@@ -1057,6 +1057,51 @@ function curl_post($url, $post_data){
 }
 
 
+/**
+ * 上面两个有异常，兼容性也不好，自已经写一个。BY Joe @2019/05/08
+ * @param $url 访问的URL
+ * @param string $post    表单数据(不填则为GET)
+ * @param string $cookie  提交的$cookies
+ * @param int $returnCookie  是否返回$cookies
+ * @return bool|string
+ */
+function curl_request($url,$post='',$cookie='', $returnCookie=0){
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+    //curl_setopt($curl, CURLOPT_REFERER, $_SERVER['']);
+    if($post) {
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+    }
+    if($cookie) {
+        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+    }
+    curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    $data = curl_exec($curl);
+    if (curl_errno($curl)) {
+        return curl_error($curl);
+    }
+    curl_close($curl);
+    if($returnCookie){
+        list($header, $body) = explode("\r\n\r\n", $data, 2);
+        preg_match_all("/Set\-Cookie:([^;]*);/", $header, $matches);
+        $info['cookie']  = substr($matches[1][0], 1);
+        $info['content'] = $body;
+        return $info;
+    }else{
+        return $data;
+    }
+}
+
+
 function getTree($data, $pid=0,$sub_name='sub',$parent_name='parent_id',$id_name='id'){
     $tree = [];
     foreach($data as $k => $v){
