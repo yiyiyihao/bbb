277b737b49f510248b3a63af5daa4bd097f1f729
Joe3160 <694995669@qq.com>
2019-06-19 20:49:19 +0800

    工单详情、修改库存

--- application/api/controller/Dealer.php
+++ application/api/controller/Dealer.php
@@ -6,7 +6,10 @@
 namespace app\api\controller;
 
 
+use app\common\model\GoodsDealer;
+use app\common\model\GoodsSkuDealer;
 use app\common\model\WorkOrder;
+use app\common\model\WorkOrderLog;
 
 trait Dealer
 {
@@ -68,7 +71,7 @@ trait Dealer
     }
 
 
-    //商品列表
+    //门店商品列表
     public function getDealerGoodsList()
     {
         $this->init();
@@ -77,8 +80,8 @@ trait Dealer
         $field = 'p2.goods_id,p2.`name`,p4.`name` cate_name,p2.goods_sn,p2.thumb,p3.stock_dealer,p3.sales_dealer,p3.status';
         $join = [
             ['goods p2', 'p1.goods_id = p2.goods_id'],
-            ['goods_dealer p3', 'p3.goods_id = p2.goods_id', 'LEFT'],
-            ['goods_cate p4', 'p4.cate_id = p2.cate_id', 'LEFT'],
+            ['goods_dealer p3', 'p3.goods_id = p2.goods_id'],
+            ['goods_cate p4', 'p4.cate_id = p2.cate_id'],
         ];
         $where = [
             'p1.store_id' => $service['store_id'],
@@ -95,18 +98,20 @@ trait Dealer
             'store_id' => $user['store_id'],
             'is_del'   => 0,
         ])->sum('stock_dealer');
-        $field = 'p3.sku_sn,p3.spec_value,p2.sales_dealer,p2.stock_dealer';
+        $field = 'p2.sku_id,p3.sku_sn,p3.spec_value,p2.sales_dealer,p2.stock_dealer';
         $join = [
-            ['goods_sku_dealer p2', 'p1.sku_id = p2.sku_id AND p2.is_del = 0 AND p2.service_id = p1.store_id AND p2.store_id=' . $user['store_id']],
+            ['goods_sku_dealer p2', 'p1.sku_id = p2.sku_id AND p2.service_id = p1.store_id'],
             ['goods_sku p3', 'p3.sku_id = p1.sku_id', 'LEFT'],
         ];
         $where = [
             'p1.is_del'   => 0,
-            'p1.status'   => 1,
-            'p1.store_id' => $service['store_id'],
+            'p2.is_del'   => 0,
             'p3.is_del'   => 0,
+            'p1.status'   => 1,
+            'p2.status'   => 1,
+            'p1.store_id' => $service['store_id'],
+            'p2.store_id' => $user['store_id'],
         ];
-
         foreach ($result['list'] as $key => $value) {
             $where['p1.goods_id'] = $value['goods_id'];
             $detail = db('goods_sku_service')
@@ -231,6 +236,174 @@ trait Dealer
         $this->_returnMsg(recursion($result));
     }
 
+    //零售商商品上架-基于商品goods_id全部上架
+    public function dealerGoodsOnSale()
+    {
+        $this->init();
+        $user = $this->user;
+        $service = $this->service;
+        $goodsId = isset($this->postParams['goods_id']) ? $this->postParams['goods_id'] : '';
+        if ($goodsId < 0) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '请求参数错误']);
+        }
+        $goodServiceSku = db('goods_sku_service')->where([
+            'store_id' => $service['store_id'],
+            'goods_id' => $goodsId,
+            'is_del'   => 0,
+            'status'   => 1,
+        ])->select();
+        if (empty($goodServiceSku)) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '商品不存在或已经下架，请联系服务商']);
+        }
+        $max = 0;
+        $min = 0;
+        foreach ($goodServiceSku as $value) {
+            $price = $value['price_service'] + $value['install_price_service'];
+            if ($price > $max) {
+                $max = $price;
+            }
+            if ($min == 0) {
+                $min = $price;
+            }
+            if ($price < $min) {
+                $min = $price;
+            }
+            $goodsDealerSku = GoodsSkuDealer::where([
+                'service_id' => $service['store_id'],
+                'store_id'   => $user['store_id'],
+                'sku_id'     => $value['sku_id'],
+                'goods_id'   => $value['goods_id'],
+                'is_del'     => 0,
+            ])->find();
+            $where = [];
+            if (!empty($goodsDealerSku)) {
+                $where = ['id' => $goodsDealerSku['id']];
+            }
+            $data = [
+                'factory_id'   => $user['factory_id'],
+                'service_id'   => $service['store_id'],
+                'store_id'     => $user['store_id'],
+                'goods_id'     => $value['goods_id'],
+                'sku_id'       => $value['sku_id'],
+                'price_dealer' => $price,
+                'status'       => 1,
+                'is_del'       => 0,
+            ];
+            $result = (new GoodsSkuDealer)->save($data, $where);
+            if ($result === false) {
+                $this->_returnMsg(['errCode' => 1, 'errMsg' => '系统故障，上架失败']);
+            }
+        }
+        $goodsDealer = GoodsDealer::where([
+            'factory_id' => $user['factory_id'],
+            'service_id' => $service['store_id'],
+            'store_id'   => $user['store_id'],
+            'goods_id'   => $goodsId,
+            'is_del'     => 0,
+        ])->find();
+        $where = [];
+        if (!empty($goodsDealer)) {
+            $where = ['id' => $goodsDealer['id']];
+        }
+        $data = [
+            'factory_id'       => $user['factory_id'],
+            'service_id'       => $service['store_id'],
+            'store_id'         => $user['store_id'],
+            'goods_id'         => $goodsId,
+            'min_price_dealer' => $min,
+            'max_price_dealer' => $max,
+            'is_del'           => 0,
+            'status'           => 1,
+            'is_del'           => 0,
+        ];
+        $result = (new GoodsDealer())->save($data, $where);
+        if ($result === false) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '系统故障，上架失败']);
+        }
+        $this->_returnMsg(['msg' => '上架成功']);
+    }
+
+    //更新库存
+    public function dealerUpdateStock()
+    {
+        $this->init();
+        $user = $this->user;
+        $service = $this->service;
+        $skuId = isset($this->postParams['sku_id']) ? $this->postParams['sku_id'] : [];
+        $stock = isset($this->postParams['stock']) ? $this->postParams['stock'] : [];
+        if (empty($skuId)) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '商品规格不能空']);
+        }
+
+        if (empty($stock)) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '库存数量不能空']);
+        }
+
+        if (count($skuId) != count($stock)) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '商品规格与库存对应不正确']);
+        }
+        foreach ($skuId as $k => $v) {
+            if ($v < 0) {
+                continue;
+            }
+            $result = GoodsSkuDealer::where([
+                'service_id' => $service['store_id'],
+                'store_id'   => $user['store_id'],
+                'sku_id'     => $v,
+                'is_del'     => 0,
+            ])->find();
+            if (empty($result)) {
+                continue;
+            }
+            $result->stock_dealer = isset($stock[$k]) && $stock[$k] > 0 ? $stock[$k] : 0;
+            $result->save();
+        }
+        $this->_returnMsg(['msg' => '更新成功']);
+    }
+
+    //零售商商品下架-基于商品goods_id全部下架
+    public function dealerGoodsOffSale()
+    {
+        $this->init();
+        $user = $this->user;
+        $service = $this->service;
+        $goodsId = isset($this->postParams['goods_id']) ? $this->postParams['goods_id'] : '';
+        if ($goodsId < 0) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '请求参数错误']);
+        }
+        $goodsDealer = GoodsDealer::where([
+            'factory_id' => $user['factory_id'],
+            'service_id' => $service['store_id'],
+            'store_id'   => $user['store_id'],
+            'goods_id'   => $goodsId,
+            'is_del'     => 0,
+        ])->find();
+        if (empty($goodsDealer)) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '商品不存在或已删除']);
+        }
+        if ($goodsDealer->status == 0) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '商品已下架']);
+        }
+        $goodsDealer->status = 0;
+        if ($goodsDealer->save() === false) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '系统故障，下架失败']);
+        }
+        $result = GoodsSkuDealer::where([
+            'service_id' => $service['store_id'],
+            'store_id'   => $user['store_id'],
+            'goods_id'   => $goodsId,
+            'is_del'     => 0,
+        ])->update([
+            'status'      => 0,
+            'update_time' => time(),
+        ]);
+        if ($result === false) {
+            $this->_returnMsg(['errCode' => 1, 'errMsg' => '系统故障，下架失败']);
+        }
+        $this->_returnMsg(['msg' => '下架成功']);
+    }
+
+
     //工单列表
     public function getDealerWorkOrderList()
     {
@@ -271,29 +444,56 @@ trait Dealer
         $user = $this->user;
         $service = $this->service;
         $worderSn = isset($this->postParams['worder_sn']) ? $this->postParams['worder_sn'] : '';
-        $field = 'p1.worder_id,p1.worder_sn,p1.work_order_type,p1.work_order_status,p1.user_name,p1.phone,p1.appointment,p1.appointment_confirm,p1.region_name,p1.address,p2.cate_id';
+        $field = 'p1.worder_id,p1.worder_sn,p1.goods_id,p1.sku_id,p1.work_order_type,p1.work_order_status,p1.user_name,p1.phone,p1.appointment,p1.appointment_confirm,p1.region_name,p1.address,p2.cate_id';
         $workOrder = new WorkOrder;
-        $result['info'] = $workOrder->alias('p1')->field($field)->join('goods p2', 'p2.goods_id=p1.goods_id')->where([
-            'p1.worder_sn'     => $worderSn,
-            'p1.is_del'        => 0,
-            'p1.status'        => 1,
-            'p1.post_store_id' => $user['store_id'],
-        ])->find();
-        if (!$result['info']) {
+        $result['work_info'] = $workOrder
+            ->alias('p1')
+            ->field($field)
+            ->join([
+                ['goods p2', 'p2.goods_id=p1.goods_id'],
+                ['goods_sku p3', 'p3.sku_id=p1.sku_id', 'LEFT'],
+            ])
+            ->where([
+                'p1.worder_sn'     => $worderSn,
+                'p1.is_del'        => 0,
+                'p1.status'        => 1,
+                'p1.post_store_id' => $user['store_id'],
+            ])->find();
+        if (!$result['work_info']) {
             $this->_returnMsg(['errCode' => 1, 'errMsg' => '工单不存在']);
         }
         $config = $workOrder->getConfigLogDetail([
-            'work_order_type' => $result['info']['work_order_type'],
-            'worder_id'       => $result['info']['worder_id'],
-            'cate_id'         => $result['info']['cate_id'],
+            'work_order_type' => $result['work_info']['work_order_type'],
+            'worder_id'       => $result['work_info']['worder_id'],
+            'cate_id'         => $result['work_info']['cate_id'],
             'factory_id'      => $user['factory_id'],
         ]);
-        $addInfo=array_shift($config);
-        p($addInfo,1);
-        p($config);
-        //p($result, 1);
-        p($config);
+        $result['work_info']['appointment']=time_to_date($result['work_info']['appointment'],'Y-m-d H:i');
+        $result['work_info']['appointment_confirm']=time_to_date($result['work_info']['appointment_confirm'],'Y-m-d H:i');
+        unset($result['work_info']['worder_id']);
+        unset($result['work_info']['goods_id']);
+        unset($result['work_info']['sku_id']);
+        unset($result['work_info']['cate_id']);
+        $addInfo = array_shift($config);
+        $result['work_info']['add_info'] = $addInfo;
+        $result['worker_post_info'] = array_shift($config);
+        $result['assess_info'] = array_shift($config);
+        $result['score_info'] = array_shift($config);
 
+        $field = 'p1.name,p4.sku_sn,p4.spec_value,p3.real_price,p5.delivery_type,p1.thumb';
+        $join = [
+            ['work_order p2', 'p1.goods_id = p2.goods_id'],
+            ['order_sku p3', 'p3.order_sn = p2.order_sn AND p2.sku_id = p3.sku_id', 'LEFT'],
+            ['goods_sku p4', 'p2.sku_id = p4.sku_id', 'LEFT'],
+            ['order p5', 'p5.order_sn = p2.order_sn', 'LEFT'],
+        ];
+        $goods = db('goods')->alias('p1')->field($field)->join($join)->where([
+            'p2.worder_sn'=>$worderSn
+        ])->find();
+        $result['goods_info'] = $goods;
+        $log= WorkOrderLog::field('add_time,nickname,action,msg')->where(['worder_sn'=>$worderSn])->order('add_time DESC')->select();
+        $result['work_log'] =$log;
+        $this->_returnMsg(recursion($result));
 
     }
 
